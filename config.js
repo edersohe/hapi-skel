0223c74eaccdf794063a9939a52f058f0330c4b2
Eder Sosa <eder.sohe@gmail.com>
2015-04-17 00:03:31 -0500

    change config initializer

--- config.js
+++ config.js
@@ -1,15 +1,9 @@
-module.exports = {
-
-  PORT: process.env.PORT || 3000,
-
-  NODE_ENV: process.env.NODE_ENV || 'development',
-
-  GOOD_CONSOLE: process.env.GOOD_CONSOLE || 'log,response,request,error',
-
-  SWAGGER_URI: process.env.SWAGGER_URI || 'http://localhost:' + this.PORT,
-
-  TV_DEBUG: process.env.TV_DEBUG || 1,
-
-  LOG_LEVEL: process.env.LOG_LEVEL || 'INFO'
-
-};
+module.exports = function() {
+  this.PORT = process.env.PORT || 3000;
+  this.GOOD_CONSOLE = process.env.GOOD_CONSOLE || 'log,response,request,error';
+  this.NODE_ENV = process.env.NODE_ENV || 'development';
+  this.SWAGGER_URI = process.env.SWAGGER_URI || 'http://localhost:' + this.PORT;
+  this.TV_DEBUG = process.env.TV_DEBUG || 1;
+  this.LOG_LEVEL = process.env.LOG_LEVEL || 'INFO';
+  return this;
+}();
